0a0a24f74b2b6e64f9c0a2913eb4297d8d6caf4c
HodayaZackay <88848337+hodayazac@users.noreply.github.com>
2022-07-20 14:46:15 +0300

    added new todo, remove mock data

--- src/app/components/new-todo/new-todo.component.ts
+++ src/app/components/new-todo/new-todo.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Form } from '@angular/forms';
+import {  NgForm } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { ITodo } from 'src/app/models/todo.interface';
+import { TosoService } from 'src/app/services/toso.service';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-new-todo',
@@ -8,16 +12,33 @@ import { Form } from '@angular/forms';
 })
 export class NewTodoComponent implements OnInit {
 
-  @ViewChild('f') form: Form;
+  @ViewChild('f') form: NgForm;
+  
 
-  constructor() { }
+  constructor(public dialog:MatDialog, private todoService:TosoService) { }
 
   ngOnInit(): void {
   }
 
   public onNewTodoSubmit():void{
+
+    const value=this.form.form.value
+    const newTodo:ITodo={
+      id:uuidv4(),
+      title:value.title,
+      description:value.description,
+      isArchived:false,
+      isCompleted:false,
+      selected:false,
+      endDate:value.endDate
+
+
+    }
     console.log("onSubmit")
     console.log(this.form)
+    this.todoService.addNewTodo(newTodo)
+    this.dialog.closeAll()
+
   }
 
 }
--- src/app/services/toso.service.ts
+++ src/app/services/toso.service.ts
@@ -6,19 +6,11 @@ import { ITodo } from '../models/todo.interface';
   providedIn: 'root'
 })
 export class TosoService {
-  private mock: ITodo[] = [
-    { "id":1, "title": "Blue-breasted cordon bleu", "description": "Uraeginthus angolensis", "isCompleted": true, "isArchived": false, "endDate": "6/6/2022", selected:true },
-    { "id":2,"title": "Royal tern", "description": "Thalasseus maximus", "isCompleted": false, "isArchived": false, "endDate": "6/26/2022" ,selected:false},
-    { "id":3,"title": "Mountain lion", "description": "Felis concolor", "isCompleted": false, "isArchived": false, "endDate": "3/25/2022",selected:false },
-    { "id":4,"title": "Whale, long-finned pilot", "description": "Globicephala melas", "isCompleted": true, "isArchived": false, "endDate": "4/4/2022" ,selected:false},
-    {"id":5, "title": "Rhinoceros, black", "description": "Diceros bicornis", "isCompleted": false, "isArchived": true, "endDate": "8/19/2021",selected:false },
-    { "id":6,"title": "Marten, american", "description": "Martes americana", "isCompleted": true, "isArchived": false, "endDate": "2/25/2022",selected:false },
-    { "id":7,"title": "Rhea, gray", "description": "Rhea americana", "isCompleted": true, "isArchived": false, "endDate": "1/24/2022",selected:false },
-    { "id":8,"title": "Kingfisher, pied", "description": "Ceryle rudis", "isCompleted": false, "isArchived": true, "endDate": "6/24/2022" ,selected:false},
-  ]
+ 
+  private todos:ITodo[]=[]
 
-  private _todoSubject:BehaviorSubject <Array<ITodo>>=new BehaviorSubject(this.mock)
-  private _singleTodoSubject:BehaviorSubject<ITodo>=new BehaviorSubject(this.mock[0])
+  private _todoSubject:BehaviorSubject <Array<ITodo>>=new BehaviorSubject(this.todos)
+  private _singleTodoSubject:BehaviorSubject<ITodo>=new BehaviorSubject(this.todos.length ? this.todos[0]:null)
 
   constructor() { }
 
@@ -33,5 +25,11 @@ export class TosoService {
 
     this._singleTodoSubject.next(todo)
     
+  }
+  public addNewTodo(newTodo:ITodo):void{
+    console.log(newTodo)
+    const exiistingTodo:ITodo[]=this._todoSubject.value 
+    exiistingTodo.push(newTodo)
+    this._todoSubject.next(exiistingTodo)
   }
 }
